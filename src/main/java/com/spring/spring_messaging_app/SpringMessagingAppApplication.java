3cbead6088fbb41463eee9e608cf31bd6f1fb215
ayush <ayushmaurya2050@gmail.com>
2025-02-21 20:09:20 +0530

    [AYUSH KUMAR MAURYA] Add : setup spring messaging application

---
+++ src/main/java/com/spring/spring_messaging_app/SpringMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_messaging_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessagingAppApplication.class, args);
+	}
+
+}
